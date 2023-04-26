d322d277016030ac6069cdc62f9fd2c6aeb3fa3d
Lukas Haidacher <lukas.haidacher@bearingpoint.com>
2023-04-26 13:14:48 +0200

    changed code with different code smells

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -12,20 +13,20 @@ func main() {
 
 	// Create a loop that populates the array with values
 	for i := 0; i < 5; i++ {
-		myArray[i] = i + 1
+		myArray[i] = rand.Intn(10)
 	}
 
 	// Print out the array
 	fmt.Println("Here's the array:", myArray)
 
 	// Wait for 2 seconds before continuing
-	time.Sleep(2 * time.Second)
+	time.Sleep(time.Second * 2)
 
 	// Call the processArray function
-	processArray(myArray)
+	sum := processArray(myArray)
 
 	// Print out the result
-	fmt.Println("The sum of the array is:", processArray(myArray))
+	fmt.Println("The sum of the array is:", sum)
 }
 
 // Process the array and return the sum of its elements
