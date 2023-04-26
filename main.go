6273fc4d820a6afc69a026b5b4d1e586f02519e0
Lukas Haidacher <lukas.haidacher@bearingpoint.com>
2023-04-26 13:09:32 +0200

    added code with code smells

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var myArray [5]int
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Welcome to my program!")
+
+	// Create a loop that populates the array with values
+	for i := 0; i < 5; i++ {
+		myArray[i] = i + 1
+	}
+
+	// Print out the array
+	fmt.Println("Here's the array:", myArray)
+
+	// Wait for 2 seconds before continuing
+	time.Sleep(2 * time.Second)
+
+	// Call the processArray function
+	processArray(myArray)
+
+	// Print out the result
+	fmt.Println("The sum of the array is:", processArray(myArray))
+}
+
+// Process the array and return the sum of its elements
+func processArray(arr [5]int) int {
+	sum := 0
+
+	// Create a loop that calculates the sum of the array
+	for _, val := range arr {
+		sum += val
+	}
+
+	// Return the sum
+	return sum
 }
